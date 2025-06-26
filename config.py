9f2523fbf85ce87cc7095d78305a3a3da2b9e773
poper12 <samyakdhengle89@gmail.com>
2025-06-27 00:17:35 +0530

    config.py

--- config.py
+++ config.py
@@ -4,18 +4,18 @@ env_vars = {
   # Get From my.telegram.org
   "API_ID": "20445873",
   #Get For @BotFather
-  "BOT_TOKEN": "7857406628:AAGT41Xak2UxFaIFDCAGYEq4uh-lt6NkFRY",
+  "BOT_TOKEN": "7555052063:AAF7qf6x_xRyuIx_8FYIkIL_f05E0VM6A4I",
   # Get For tembo.io
   
   "DATABASE_URL_PRIMARY": "mongodb+srv://narutouzumaki22551:narutouzumaki22551@cluster0.econe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "logs_hai_bhai",
+  "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
-  "CHANNEL": "Manhwa_Weebs",
+  "CHANNEL": "",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "[{chap_num}] [MW] {chap_name} [@Manhwa_Weebs]"
+  "FNAME": "[MC] [{chap_num}] {chap_name} @Manga_Campus"
 }
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL')
